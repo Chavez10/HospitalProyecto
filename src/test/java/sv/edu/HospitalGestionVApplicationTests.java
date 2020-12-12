ace12293ebc8140846f0987cfb60cee6cb47ee37
Chavez10 <davidchavez1098@outlook.com>
2020-12-12 03:11:15 -0600

    First commit

---
+++ src/test/java/sv/edu/HospitalGestionVApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalGestionVApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
