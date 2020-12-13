f28272c1aa66bbdcdebf4ae6e958d3d57e1a1cc0
Chavez10 <davidchavez1098@outlook.com>
2020-12-12 20:30:48 -0600

    Modificacion del pom.xml y modelos

---
+++ src/main/java/sv/edu/model/NacionalidadesModel.java
@@ -0,0 +1,10 @@
+package sv.edu.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class NacionalidadesModel {
+	
+	private int idnacionalidad;
+	private String nacionalidad;
+}
---
+++ src/main/java/sv/edu/model/TiposDocumentosModel.java
@@ -0,0 +1,20 @@
+package sv.edu.model;
+
+public class TiposDocumentosModel {
+	private int idtipodocu;
+	private String documento;
+	public int getIdtipodocu() {
+		return idtipodocu;
+	}
+	public void setIdtipodocu(int idtipodocu) {
+		this.idtipodocu = idtipodocu;
+	}
+	public String getDocumento() {
+		return documento;
+	}
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+	
+	
+}
