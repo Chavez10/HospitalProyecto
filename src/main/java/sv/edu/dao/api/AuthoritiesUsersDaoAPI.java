58f0737d4fb7f0edad7399cb18ebba5e2c3fb489
Chavez10 <davidchavez1098@outlook.com>
2020-12-13 20:20:00 -0600

    Eliminacion de authorities_users

--- src/main/java/sv/edu/dao/api/AuthoritiesUsersDaoAPI.java
+++
@@ -1,9 +0,0 @@
-package sv.edu.dao.api;
-
-import org.springframework.data.repository.CrudRepository;
-
-import sv.edu.model.AuthoritiesUsers;
-
-public interface AuthoritiesUsersDaoAPI extends CrudRepository<AuthoritiesUsers, Long> {
-
-}
--- src/main/java/sv/edu/services/api/AuthoritiesUsersServiceAPI.java
+++
@@ -1,8 +0,0 @@
-package sv.edu.services.api;
-
-import sv.edu.commons.GenericServiceAPI;
-import sv.edu.model.AuthoritiesUsers;
-
-public interface AuthoritiesUsersServiceAPI extends GenericServiceAPI<AuthoritiesUsers, Long> {
-
-}
